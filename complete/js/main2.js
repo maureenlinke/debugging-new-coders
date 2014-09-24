908e0d1e7055d6b0f9777363a00e64b3e5f08225
Maureen Linke <maureenlinke@gmail.com>
2014-09-24 00:17:36 -0400

    updated main2

--- complete/js/main2.js
+++ complete/js/main2.js
@@ -1,14 +1,16 @@
-$legend =     $('#map-legend');
+$legend = $('#map-legend');
 
 function showLegend() {
 
-    var colors =   [ "#409e94","#7e7e7e" ];
-    var txt =    [
-    '40% or more',
-    '39% or fewer' ];
+    var colors = ["#409e94", '#9FD8D2', '#7e7e7e'];
+    var txt = [
+        '45% or more',
+        '44 - 38%',
+        '39% or fewer'
+    ];
     var htm = '<h5>Pct. of obese children</h5><ul>';
     var cls = 'dots';
-    for ( var i = 0; i < colors.length; i++ ) {
+    for (var i = 0; i < colors.length; i++) {
         htm += '<li id="leg' + i + '" class="legends"><span class="' + cls + '" style="background:' + colors[i] + '"></span>' + txt[i] + '</li>';
     }
     htm += '</ul>';
@@ -19,8 +21,8 @@ function showLegend() {
 //access token
 L.mapbox.accessToken = 'pk.eyJ1IjoibWF1cmVlbmxpbmtlIiwiYSI6IkRKWWtHcHMifQ.QcjqScXhjOanYPFAvnLZaA';
 
-  // set default view -- lat, long, zoom
-var map = L.mapbox.map('map', 'examples.map-20v6611k').setView([42.5,-73.04], 20);
+// set default view -- lat, long, zoom
+var map = L.mapbox.map('map', 'examples.map-20v6611k').setView([42.5, -73.04], 20);
 
 // map markerLayer from external geojson file
 var markerLayer = L.mapbox.featureLayer()
@@ -30,28 +32,35 @@ var markerLayer = L.mapbox.featureLayer()
 
 
 
- markerLayer.on('ready', function(layer) {
-        //Customizing marker styles based on an attribute.
+markerLayer.on('ready', function(layer) {
+    //Customizing marker styles based on an attribute.
 
-        this.eachLayer(function(marker) {
-            if (marker.toGeoJSON().properties.pct_over_obese > 40) {
-                marker.setIcon(L.mapbox.marker.icon({
-                    'marker-color': '#409e94',
-                    'marker-size': 'medium'
-                }));
-            } else {
-                marker.setIcon(L.mapbox.marker.icon({
-                	'marker-color': '#7e7e7e',
-                    'marker-size': 'medium'
-                }));
-            }
-            // Bind a popup to each icon based on the same properties
-            marker.bindPopup(marker.toGeoJSON().properties.school + '</br>'+
-              marker.toGeoJSON().properties.pct_over_obese + '% ') ;
-        });
-        // // zooms the map zoom to fit all markers in your geojson file in view
+    this.eachLayer(function(marker) {
+        if (marker.toGeoJSON().properties.pct_over_obese > 45) {
+            marker.setIcon(L.mapbox.marker.icon({
+                'marker-color': '#409e94',
+                'marker-size': 'medium'
+            }));
+        } else if (marker.toGeoJSON().properties.pct_over_obese > 35) {
+            marker.setIcon(L.mapbox.marker.icon({
+                'marker-color': '#9FD8D2',
+                'marker-size': 'medium'
+            }));
+        } else {
+            marker.setIcon(L.mapbox.marker.icon({
+                'marker-color': '#7e7e7e',
+                'marker-size': 'medium'
+            }));
+        }
+        // Bind a popup to each icon based on the same properties
+        marker.bindPopup(marker.toGeoJSON().properties.school + '</br>' +
+            marker.toGeoJSON().properties.pct_over_obese + '% ');
+    });
+    // // zooms the map zoom to fit all markers in your geojson file in view
 
-         map.fitBounds(markerLayer.getBounds(), { padding: [5, 5] });
-    })
+    map.fitBounds(markerLayer.getBounds(), {
+        padding: [5, 5]
+    });
+})
     .addTo(map);
-    showLegend();
+showLegend();
--- start/js/main2.js
+++ start/js/main2.js
@@ -1,29 +1,29 @@
-//create legend
-
-$legend =     $('#legend');
+$legend = $('.map-legend');
 
 function showLegend() {
 
-    var txt =    [
-    '40% or more',
-    '39% or fewer' ];
+
+    var txt = [
+        '45% or more',
+        '44 - 38%',
+        '39% or fewer'
+    ];
     var htm = '<h5>Pct. of obese children</h5><ul>';
     var cls = 'dots';
-    for ( var i = 0; i < cols.length; i++ ) {
-        htm += '<li id="leg' + i + '" class="legends"><span class="' + cls + '" style="background:' + cols[i] + '"></span>' + txt[i] + '</li>';
-        var cols =   [ "#409e94","#7e7e7e" ];
-
+    for (var i = 0; i < colors.length; i++) {
+        htm += '<li id="leg' + i + '" class="legends"><span class="' + cls + '" style="background:' + colors[i] + '"></span>' + txt[i] + '</li>';
+        var colors = ["#409e94", '#9FD8D2', '#7e7e7e'];
     }
     htm += '</ul>';
-    $legend.html(htm);
+    $map-legend.html(htm);
 
 };
 
 //access token
 L.mapbox.accessToken = 'pk.eyJ1IjoibWF1cmVlbmxpbmtlIiwiYSI6IkRKWWtHcHMifQ.QcjqScXhjOanYPFAvnLZaA';
 
-  // set default view -- lat, long, zoom
-var map = L.mapbox.map('map', 'examples.map-20v6611k').setView([42.5,-73.04], 20);
+// set default view -- lat, long, zoom
+var map = L.mapbox.map('map', 'examples.map-20v6611k').setView([42.5, -73.04], 20);
 
 // map markerLayer from external geojson file
 var markerLayer = L.mapbox.featureLayer()
@@ -31,28 +31,36 @@ var markerLayer = L.mapbox.featureLayer()
     .addTo(map);
 
 
- markerLayer.on('ready', function(layer) {
-        //Customizing marker styles based on an attribute.
 
-        this.eachLayer(function(marker) {
-            if (marker.toGeoJSON().properties.pct_over_obese ==! 40) {
-                marker.setIcon(L.mapbox.marker.icon({
-                    'marker-color': '#409e94',
-                    'marker-size': 'medium'
-                }));
-            } else {
-                marker.setIcon(L.mapbox.marker.icon({
-                    'marker-color': '#7e7e7e',
-                    'marker-size': 'medium'
-                }));
 
-            // Bind a popup to each icon based on the same properties
-            marker.bindPopup(marker.toGeoJSON().properties.school + '</br>'+
-              marker.toGeoJSON().properties.pct_obese + '% ') ;
-        });
-        // // zooms the map zoom to fit all markers in your geojson file in view
+markerLayer.on('ready', function(layer) {
+    //Customizing marker styles based on an attribute.
 
-         map.fitBounds(markerLayer.getBounds(), { padding: [5, 5] });
-    })
+    this.eachLayer(function(marker) {
+        if (marker.toGeoJSON().properties.pct_over_obese =! 45) {
+            marker.setIcon(L.mapbox.marker.icon({
+                'marker-color': '#409e94',
+                'marker-size': 'medium'
+            }));
+        } else  (marker.toGeoJSON().properties.pct_over_obese > 35) {
+            marker.setIcon(L.mapbox.marker.icon({
+                'marker-color': '#9FD8D2',
+                'marker-size': 'medium'
+            }));
+        } else if{
+            marker.setIcon(L.mapbox.marker.icon({
+                'marker-color': '#7e7e7e',
+                'marker-size': 'medium'
+            }));
+        }
+        // Bind a popup to each icon based on the same properties
+        marker.bindPopup(marker.toGeoJSON().properties.school + '</br>' +
+            marker.toGeoJSON().properties.pct_over_obese + '% ');
+    });
+    // // zooms the map zoom to fit all markers in your geojson file in view
+
+    map.fitBounds(markerLayer.getBounds(), {
+        padding: [5, 5]
+    });
+})
     .addTo(map);
- 
